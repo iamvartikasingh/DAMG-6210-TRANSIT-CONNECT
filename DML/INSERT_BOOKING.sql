e57b38c3132664c967678d6d26f9ce676e8626d0
balajiki1 <balaji.ki@northeastern.edu>
2025-03-24 13:20:56 -0400

    commit changes

--- DML/INSERT_BOOKING.sql
+++ DML/INSERT_BOOKING.sql
@@ -1,7 +1,7 @@
 BEGIN
     -- Delete existing data
     DELETE FROM BOOKING;
-
+    COMMIT;
     -- Insert fresh data
     INSERT INTO BOOKING VALUES (1, TO_DATE('2024-01-01', 'YYYY-MM-DD'), 2, 'Y', 100.00, 90.00, 1, 1, 1);
     INSERT INTO BOOKING VALUES (2, TO_DATE('2024-01-05', 'YYYY-MM-DD'), 1, 'N', 50.00, 50.00, 2, 2, 2);
